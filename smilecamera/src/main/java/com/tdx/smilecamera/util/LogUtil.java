8806524c27ec5c0df54948ed3796bd40de4d779f
yj <yj@selobj.com>
2019-06-03 17:17:28 +0800

    导包2

--- smilecamera/src/main/java/com/tdx/smilecamera/util/LogUtil.java
+++ smilecamera/src/main/java/com/tdx/smilecamera/util/LogUtil.java
@@ -2,7 +2,6 @@ package com.tdx.smilecamera.util;
 
 import android.util.Log;
 
-import static com.cjt2325.cameralibrary.BuildConfig.DEBUG;
 
 /**
  * =====================================
@@ -22,17 +21,14 @@ public class LogUtil {
     }
 
     public static void v(String tag, String msg) {
-        if (DEBUG)
             Log.v(tag, msg);
     }
 
     public static void d(String tag, String msg) {
-        if (DEBUG)
             Log.d(tag, msg);
     }
 
     public static void e(String tag, String msg) {
-        if (DEBUG)
             Log.e(tag, msg);
     }
 
